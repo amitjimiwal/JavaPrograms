8b7d5d3d2f51d835d4df4cbedd8e58795864cba2
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-16 11:18:41 +0530

    intro

--- OOPS/Intro.java
+++ OOPS/Intro.java
@@ -1,12 +1,27 @@
 package OOPS;
+/*
+Class is a named group of properties and functions,which are used to create objects. Class is an template of an Object
+Class : class <class_name>{
+      properties
+      methods
+}
+
+Objects: real world entity. Instance of a class. Made dynamically using new Keyword.
+*/
 
 class Student{
+      // properties
       int roll;
       String name;
       float marks;
 }
+class A{
+
+}
 public class Intro {
       public static void main(String[] args) {
+            // creating a object(created at runtime in heap memory)
+            // reference variable s1 in stack memory points to this object in heap of student class
             Student s1=new Student();
             s1.roll=10;
             s1.marks=88;
