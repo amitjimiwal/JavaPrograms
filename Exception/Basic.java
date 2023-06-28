81154df49d8b48aae5be5178d59542f98963890f
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-28 22:50:02 +0530

    array index out of bounds exxception

--- Exception/Basic.java
+++ Exception/Basic.java
@@ -5,15 +5,17 @@ import java.nio.channels.FileLockInterruptionException;
 
 class Basic{
       public static void main(String[] args) {
-            
+            int[] a=new int[]{1,2,3,4};
             try{
               
             System.out.println("S1");
             // System.out.println("S2");
-             System.out.println(0/0); 
+            //  System.out.println(0/0); 
+            System.out.println(a[10]);
             System.out.println("S3");
             }catch(Exception obj){
                 System.out.println("S4");
+                System.out.println(obj);
             }
           
             System.out.println("S2");
