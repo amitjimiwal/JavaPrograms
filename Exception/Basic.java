74d58bc00862ab5858ccc1690daf6b737577f19f
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-28 22:40:53 +0530

    example2

--- Exception/Basic.java
+++ Exception/Basic.java
@@ -1,12 +1,19 @@
 package Exception;
 
+import java.io.FileNotFoundException;
+import java.nio.channels.FileLockInterruptionException;
+
 class Basic{
       public static void main(String[] args) {
-            System.out.println("S1");
+            
             try{
-               System.out.println(0/0); 
-            }catch(ArithmeticException obj){
-                System.out.println(obj);
+              
+            System.out.println("S1");
+            // System.out.println("S2");
+             System.out.println(0/0); 
+            System.out.println("S3");
+            }catch(Exception obj){
+                System.out.println("S4");
             }
           
             System.out.println("S2");
@@ -15,6 +22,6 @@ class Basic{
             // S1 
             // Exception in thread "main" java.lang.ArithmeticException: / by zero
             // abnormal termination
-            System.out.println("example of unchecked exception");
+            System.out.println("example of  exception");
       }
 }
