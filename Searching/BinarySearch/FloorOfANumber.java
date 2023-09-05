59edaf63c58d99380fb51722534fa6f0db7cf338
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-09-05 18:05:52 +0530

    floor

--- Searching/BinarySearch/FloorOfANumber.java
+++ Searching/BinarySearch/FloorOfANumber.java
@@ -1,14 +1,11 @@
 package Searching.BinarySearch;
-/*
- Given an array of positive integers and you have to find the ceiling of target in the array
- Ceiling of a number is the smallest number greater than or equal to that number;
- */
-public class CeilingOfANumber {
+
+public class FloorOfANumber {
      public static void main(String[] args) {
           int[] arr={2,3,5,9,14,16,18};
-          System.out.println(ceilingNumber(arr, 17));
+          System.out.println(floorNumber(arr, 17));
      }
-     static int ceilingNumber(int[] arr,int target){
+     static int floorNumber(int[] arr,int target){
           int start=0;
           int end=arr.length-1;
           while(start<=end){
@@ -17,6 +14,6 @@ public class CeilingOfANumber {
                else if(arr[mid]>target) end=mid-1;
                else start=mid+1;
           }
-          return start;
-     }
+          return end;
+     }   
 }
