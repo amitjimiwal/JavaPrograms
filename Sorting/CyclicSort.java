65b8a8d506972c57e945fa514145bd2271a402f4
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-09-13 16:14:51 +0530

    CYclic Sort

---
+++ Sorting/CyclicSort.java
@@ -0,0 +1,31 @@
+/*
+ * To apply Cyclic Sort we have Conditions: Array should have elements from 1to N for array of size N
+ * O(N) T.C for all the Cases- Best,Worst and Average
+ */
+
+import java.util.Arrays;
+
+public class CyclicSort {
+     public static void main(String[] args) {
+          int[] arr = { 5, 3, 4, 1, 2 };
+          cyclicSort(arr);
+          System.out.println(Arrays.toString(arr));
+     }
+
+     static void cyclicSort(int[] arr) {
+          int i = 0;
+          while (i < arr.length) {
+              int correct = arr[i] - 1;
+              if (arr[i] != arr[correct]) {
+                  swap(arr, i , correct);
+              } else {
+                  i++;
+              }
+          }
+     }
+     static void swap(int[] arr, int first, int second) {
+          int temp = arr[first];
+          arr[first] = arr[second];
+          arr[second] = temp;
+      }
+}
