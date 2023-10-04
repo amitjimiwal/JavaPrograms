5145049063bc9ab6cab0931e27f01de2b57db06a
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-10-04 12:56:39 +0530

    strings and prety formatting

---
+++ Strings/First.java
@@ -0,0 +1,17 @@
+package Strings;
+/*
+ * String is a non Primitive data type/ reference data type
+ * It's basically a collection of character.
+ * Immutable
+ */
+public class First {
+     public static void main(String[] args) {
+          //Types of declaring String
+          String s="Amit";
+          String a=new String("Amit");
+
+          //pretty formaating in the strings
+          System.out.printf("Hello my name is %s and %s",s,a);
+          System.out.println();
+     }
+}
