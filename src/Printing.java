7ce5480a451dd1530eba0f5099c5753c62b12cf5
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2023-11-11 15:27:55 +0530

    preety printing

---
+++ src/Printing.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Printing {
+     public static void main(String[] args) {
+          int a=10;
+          //I have to make something like a template string literal of javascript in Java.
+          System.out.printf("Your marks is %i ",a); //Your marks is a.
+     }
+}
