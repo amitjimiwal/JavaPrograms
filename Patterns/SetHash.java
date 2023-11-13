dec1ed74740d9400a8daf5d331272ec6398a1dd7
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2023-11-13 20:29:23 +0530

    set hashset

---
+++ Patterns/SetHash.java
@@ -0,0 +1,42 @@
+package Patterns;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class SetHash {
+     public static void main(String[] args) {
+          Set<String> names=new HashSet<>();
+          names.add("amit");
+          names.add("Jimiwal");
+          names.add("Jimiwal2");
+          names.add("walter");
+          names.remove("Walter");
+          // names.clear();
+          System.out.println(names.isEmpty());
+
+          //iterating through a set
+          for (String string : names) {
+               System.out.println(string);
+          }
+
+
+          //set doesn't contain duplicate but List contains duplicates
+          //set don't have any order it can put elements in any order.
+          List<Integer> numbers=new ArrayList<>();
+          numbers.add(1);
+          numbers.add(2);
+          numbers.add(2);
+          numbers.add(1);
+          System.out.println(numbers);
+
+          //converting them into set 
+          Set<Integer> numset=new HashSet<>();
+          numset.addAll(numbers);
+          System.out.println(numset);
+
+     }
+}
