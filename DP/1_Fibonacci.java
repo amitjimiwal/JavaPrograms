55b80265a372187a5aa13ebac3281a6afa0d7caf
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-07-03 12:05:47 +0530

    DP

---
+++ DP/1_Fibonacci.java
@@ -0,0 +1,17 @@
+class FibonacciDP{
+    public static void main(String[] args) {
+        System.out.println("Fibonacci DP solution");
+    }
+
+    static int memoizedDP(int n,long[] dp){
+        return 1;
+    }
+
+    static int tabulationDP(int n){
+        return 1;
+    }
+
+    static int optimisedSpace(int n){
+        return 1;
+    }
+}
