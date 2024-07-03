e6f0f51d62d70d7b6b613c869c139fdca64249e7
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-07-03 16:27:03 +0530

    stairs and fibo

--- DP/2_ClimbingStairs.java
+++ DP/2_ClimbingStairs.java
@@ -1,18 +1,16 @@
 import java.util.*;
 
-class FibonacciDP{
+class  ClimbingStairs{
     public static void main(String[] args) {
-        System.out.println("Fibonacci DP solution");
-        int n=5;
+        int n=4;
         int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
         System.out.println(memoizedDP(n,dp));
         System.out.println(tabulationDP(n));
         System.out.println(optimisedSpace(n));
     }
-
     static int memoizedDP(int n,int[] dp){
-        if(n<=1) return n;
+        if(n<=1) return 1;
 
         //check for overlapping subproblem
         if(dp[n]!=-1) return dp[n];
@@ -23,7 +21,7 @@ class FibonacciDP{
     }
     static int tabulationDP(int n){
         int[] dp=new int[n+1];
-        dp[0]=0;
+        dp[0]=1;
         dp[1]=1;
         for (int i = 2; i <=n; i++) {
             dp[i]=dp[i-1]+dp[i-2];
@@ -32,7 +30,7 @@ class FibonacciDP{
     }
 
     static int optimisedSpace(int n){
-        int p1=0;
+        int p1=1;
         int p2=1;
         if(n<=1) return n;
         for (int i = 2; i <=n; i++) {
